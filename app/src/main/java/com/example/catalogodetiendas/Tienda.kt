19c5d75b458a7e4d6e9bcc7887320ed124dc521a
angel <angelestrella364@gmail.com>
2023-05-23 19:41:05 -0600

    Initial commit

---
+++ app/src/main/java/com/example/catalogodetiendas/Tienda.kt
@@ -0,0 +1,9 @@
+package com.example.catalogodetiendas
+
+import java.util.UUID
+
+data class Tienda(
+    val id: UUID,
+    val nombre:String,
+    val telefono:String
+)
---
+++ app/src/main/java/com/example/catalogodetiendas/TiendaListFragment.kt
@@ -0,0 +1,45 @@
+package com.example.catalogodetiendas
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.catalogodetiendas.databinding.FragmentTiendaListBinding
+
+private const val TAG = "TiendaListFragment"
+class TiendaListFragment : Fragment() {
+    private val tiendaListViewModel : TiendaListViewModel by viewModels()
+    private var _binding: FragmentTiendaListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding){
+            "No se puede acceder al binding"
+        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG,"Existen "+tiendaListViewModel.tiendas.size+" tiendas")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTiendaListBinding.inflate(inflater,container,false)
+        val tiendas = tiendaListViewModel.tiendas
+        val adapter = TiendaListAdapter(tiendas)
+
+        binding.tiendaRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.tiendaRecyclerView.adapter = adapter
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
